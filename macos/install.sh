ff03705c293d06acb8cdef88c91fa49cf79a4ce3
kwguo <shana0440@gmail.com>
2022-08-13 10:59:48 +0800

    macos: change default shell to fish

--- macos/install.sh
+++ macos/install.sh
@@ -10,3 +10,6 @@ stow vscode --target="$HOME/Library/Application Support/Code/User/"
 bash ./vscode/install-extensions.sh
 
 bash ./macos/appearance.sh
+
+echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
+chsh -s /opt/homebrew/bin/fish
